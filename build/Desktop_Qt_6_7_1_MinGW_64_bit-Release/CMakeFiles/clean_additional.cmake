8442db7d51e8a47c6a98312357f376f739f89d6a
NoX <128933018+NN0X@users.noreply.github.com>
2024-05-30 15:31:46 +0200

    initial commit

---
+++ build/Desktop_Qt_6_7_1_MinGW_64_bit-Release/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,10 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Release")
+  file(REMOVE_RECURSE
+  "CMakeFiles\\Qtify_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\Qtify_autogen.dir\\ParseCache.txt"
+  "Qtify_autogen"
+  )
+endif()
